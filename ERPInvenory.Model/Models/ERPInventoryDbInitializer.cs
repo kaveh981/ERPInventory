359ff7f3154e07ff20bef9c22e88a6cec2eaa4f3
kaveh fereidouni <kaveh981@gmail.com>
2015-07-31 02:37:33 -0400

    3

--- ERPInvenory.Model/Models/ERPInventoryDbInitializer.cs
+++ ERPInvenory.Model/Models/ERPInventoryDbInitializer.cs
@@ -12,31 +12,19 @@ namespace ERPInventory.Model.Models
     {
         protected override void Seed(ERPInventoryDBContext context)
         {
-            IList<Category> defualtcategories = new List<Category>();
             Category cat1, cat12, cat13, cat14, cat131, cat132;
 
-            IList<Category> categories1 = new List<Category>();
-            IList<Category> categories13 = new List<Category>();
+            cat1 = new Category() { Cat_Title = "Category1", Cat_NodeDepth = 0 };
+            cat12 = new Category() { Cat_Title = "Category12", Cat_NodeDepth = 1,Parent=cat1 };
+            cat13 = new Category() { Cat_Title = "Category13", Cat_NodeDepth = 1, Parent = cat1 };
+            cat14 = new Category() { Cat_Title = "Category14", Cat_NodeDepth = 1, Parent = cat1 };
+            cat131 = new Category() { Cat_Title = "Category131", Cat_NodeDepth = 2, Parent = cat13 };
+            cat132 = new Category() { Cat_Title = "Category132", Cat_NodeDepth = 2, Parent = cat13 };
 
-
-
-            cat12 = new Category() { Cat_Title = "Category12", Cat_NodeDepth = 0 };
-            cat13 = new Category() { Cat_Title = "Category13",  Cat_NodeDepth = 0, Categories=categories13 };
-            cat14 = new Category() { Cat_Title = "Category14",  Cat_NodeDepth = 0 };
-
-
-            cat131 = new Category() { Cat_Title = "Category131", Cat_NodeDepth = 0 };
-            cat132 = new Category() { Cat_Title = "Category132",  Cat_NodeDepth = 0 };
-
-            categories1.Add(cat12);
-            categories1.Add(cat13);
-            categories1.Add(cat14);
-            cat1 = new Category() { Cat_Title = "Category1", Cat_NodeDepth = 0, Categories = categories1 };
-
-            categories13.Add(cat131);
-            categories13.Add(cat132);
-
-            context.Categories.Add(cat1);
+            context.Categories.Add(cat12);
+            context.Categories.Add(cat14);
+            context.Categories.Add(cat131);
+            context.Categories.Add(cat132);
            
             base.Seed(context);
         }
