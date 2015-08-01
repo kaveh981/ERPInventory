c9bc5e36a8176bbcf7a69ffecb42cd14d9ff7065
kaveh fereidouni <kaveh981@gmail.com>
2015-08-01 02:53:14 -0400

    Controller Added

---
+++ ERPInventory/App_Start/NinjectConfig.cs
@@ -0,0 +1,35 @@
+﻿using ERPInvenory.DataLayer;
+using ERPInvenory.DataLayer.Repository;
+using ERPInventory.Model.Models;
+using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+namespace ERPInventory.App_Start
+{
+    public class NinjectConfig
+    {
+        public static Lazy<IKernel> CreateKernel = new Lazy<IKernel>(() =>
+        {
+            var kernel = new StandardKernel();
+            kernel.Load(Assembly.GetExecutingAssembly());
+
+            RegisterServices(kernel);
+
+            return kernel;
+        });
+
+        private static void RegisterServices(KernelBase kernel)
+        {
+            //kernel.Bind<IOrder>().To<Mapi.BusinessLayer.Order>();
+            //kernel.Bind<IItem>().To<Item>();
+            //kernel.Bind<IAccount>().To<Account>();
+            kernel.Bind<IUnitOfWork>().To<UnitOfWork>();
+            kernel.Bind(typeof(IGenericRepository<>)).To(typeof(GenericRepository<>));
+            kernel.Bind<ERPInventoryDBContext>().To<ERPInventoryDBContext>();
+        }
+    }
+}
---
+++ ERPInventory/Controllers/CategoryController1.cs
@@ -0,0 +1,49 @@
+﻿using ERPInvenory.BusinessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ERPInventory.Controllers
+{
+    public class CategoryController1 : ApiController
+    {
+        // GET api/<controller>
+
+          private readonly IHierarchyCategory _Category;
+
+
+          public CategoryController1(IHierarchyCategory category)
+        {
+            _Category = category;
+        }
+
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
