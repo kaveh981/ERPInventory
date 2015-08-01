494c85c31b0aff1305e2782a998d8df284566687
kaveh fereidouni <kaveh981@gmail.com>
2015-08-01 00:51:44 -0400

    initializing SPs

--- ERPInvenory.Model/Models/ERPInventoryDbInitializer.cs
+++ ERPInvenory.Model/Models/ERPInventoryDbInitializer.cs
@@ -8,45 +8,87 @@ using System.Collections;
 
 namespace ERPInventory.Model.Models
 {
-    public class ERPInventoryDbInitializer : DropCreateDatabaseIfModelChanges<ERPInventoryDBContext>
+    public class ERPInventoryDbInitializer : DropCreateDatabaseAlways<ERPInventoryDBContext>
     {
         protected override void Seed(ERPInventoryDBContext context)
         {
+            StringBuilder AllSubCategoryId = new StringBuilder();
+            AllSubCategoryId.Append(" create FUNCTION [dbo].[AllSubCategoryId](@CategoryId uniqueIdentifier )");
+            AllSubCategoryId.Append(" RETURNS TABLE");
+            AllSubCategoryId.Append(" AS");
+            AllSubCategoryId.Append(" RETURN (WITH CategoryChart (CategoryId,Cat_ParentId,CategoryLevel) AS");
+            AllSubCategoryId.Append(" (");
+            AllSubCategoryId.Append(" SELECT");
+            AllSubCategoryId.Append(" a.CategoryId,  a.Cat_ParentId,0");
+            AllSubCategoryId.Append(" FROM dbo.Categories as a");
+            AllSubCategoryId.Append(" WHERE a.CategoryId 	= @CategoryId");
+            AllSubCategoryId.Append(" UNION ALL");
+            AllSubCategoryId.Append(" SELECT");
+            AllSubCategoryId.Append(" a.CategoryId,  a.Cat_ParentId, b.Categorylevel+1");
+            AllSubCategoryId.Append(" FROM dbo.Categories a");
+            AllSubCategoryId.Append(" INNER JOIN CategoryChart b ON a.Cat_ParentId = b.CategoryId");
+            AllSubCategoryId.Append(" ) select CategoryId from CategoryChart );");
+            context.Database.ExecuteSqlCommand(AllSubCategoryId.ToString());
 
-//            context.Database.ExecuteSqlCommand("create FUNCTION [dbo].[AllSubCategoryId](@CategoryId uniqueIdentifier )
-//RETURNS TABLE
-//AS
-//RETURN (WITH CategoryChart (CategoryId,Cat_ParentId,CategoryLevel) AS
-// (
-//  SELECT
-//     a.CategoryId,  a.Cat_ParentId,0
-//   FROM dbo.Categories as a
-//   WHERE a.CategoryId 	= @CategoryId
-//  UNION ALL
-//  SELECT
-//     a.CategoryId,  a.Cat_ParentId, b.Categorylevel+1
-//   FROM dbo.Categories a
-//     INNER JOIN CategoryChart b ON a.Cat_ParentId = b.CategoryId
-//) select CategoryId from CategoryChart );
-//");
+            StringBuilder GetFullPathCategory = new StringBuilder();
+            GetFullPathCategory.Append(" create FUNCTION [dbo].[GetFullPathCategory](@Categoryid uniqueidentifier )");
+            GetFullPathCategory.Append(" RETURNS TABLE");
+            GetFullPathCategory.Append(" AS");
+            GetFullPathCategory.Append(" RETURN (WITH Categorychart ( CategoryID, Cat_CreateTime,Cat_NodeDepth,Cat_ParentId,Cat_Priority,Cat_StampTime,Cat_Title,Categorylevel) AS");
+            GetFullPathCategory.Append(" (");
+            GetFullPathCategory.Append(" SELECT");
+            GetFullPathCategory.Append("     a.CategoryID, a.Cat_CreateTime,a.Cat_NodeDepth,a.Cat_ParentId,a.Cat_Priority,a.Cat_StampTime,a.Cat_Title,0");
+            GetFullPathCategory.Append("   FROM dbo.Categories as a");
+            GetFullPathCategory.Append("   WHERE a.CategoryID 	= @CategoryID");
+            GetFullPathCategory.Append("  UNION ALL");
+            GetFullPathCategory.Append("  SELECT");
+            GetFullPathCategory.Append("     a.CategoryID, a.Cat_CreateTime,a.Cat_NodeDepth,a.Cat_ParentId,a.Cat_Priority,a.Cat_StampTime,a.Cat_Title, b.CategoryLevel+1");
+            GetFullPathCategory.Append("   FROM dbo.Categories a");
+            GetFullPathCategory.Append("     INNER JOIN CategoryChart b ON a.CategoryID = b.Cat_ParentId");
+            GetFullPathCategory.Append(")");
+            GetFullPathCategory.Append(" select * from Categorychart   );");
+            context.Database.ExecuteSqlCommand(GetFullPathCategory.ToString());
+
+            StringBuilder GetParentCategoryByID = new StringBuilder();
+            GetParentCategoryByID.Append("  create proc [dbo].[GetParentCategoryByID]");
+            GetParentCategoryByID.Append(" @ID uniqueidentifier,");
+            GetParentCategoryByID.Append(" @startLevel nvarchar(50)=null");
+            GetParentCategoryByID.Append(" as");
+            GetParentCategoryByID.Append(" if(@startLevel ='' or @startLevel is null or @startLevel='0')");
+            GetParentCategoryByID.Append(" set @startLevel=null; ");
+            GetParentCategoryByID.Append(" select * into #temp from dbo.GetFullPathCategory(@ID)");
+            GetParentCategoryByID.Append(" if(@startLevel is null)");
+            GetParentCategoryByID.Append(" select * from #temp order by CategoryLevel desc");
+            GetParentCategoryByID.Append(" else");
+            GetParentCategoryByID.Append(" select * from #temp where CategoryLevel<(select CategoryLevel from #temp where Category=@startLevel) order by CategoryLevel desc ");
+            GetParentCategoryByID.Append(" drop table #temp");
+            context.Database.ExecuteSqlCommand(GetParentCategoryByID.ToString());
+
+            StringBuilder GetSubCategoryByParent = new StringBuilder();
+            GetSubCategoryByParent.Append("   create proc [dbo].[GetSubCategoryByParent]");
+            GetSubCategoryByParent.Append(" @Cat_ParentId uniqueidentifier");
+            GetSubCategoryByParent.Append(" as");
+            GetSubCategoryByParent.Append(" SELECT     CategoryId");
+            GetSubCategoryByParent.Append(" FROM         Categories");
+            GetSubCategoryByParent.Append(" where Cat_ParentId in( select * from dbo.AllSubCategoryID(@Cat_ParentId))");
+            context.Database.ExecuteSqlCommand(GetSubCategoryByParent.ToString());
+
             Category cat1, cat12, cat13, cat14, cat131, cat132;
 
             IList<Category> categories1 = new List<Category>();
             IList<Category> categories2 = new List<Category>();
 
-            cat1 = new Category() { Cat_CreateTime = DateTime.Now, Cat_Title = "Category1", Cat_Priority = 1, Cat_NodeDepth = 0, Categories=categories1};
+            cat1 = new Category() { Cat_CreateTime = DateTime.Now, Cat_Title = "Category1", Cat_Priority = 1, Cat_NodeDepth = 0, Categories = categories1 };
             cat12 = new Category() { Cat_CreateTime = DateTime.Now, Cat_Title = "Category12", Cat_NodeDepth = 1, Cat_Priority = 1 };
-            cat13 = new Category() { Cat_CreateTime = DateTime.Now, Cat_Title = "Category13", Cat_NodeDepth = 1, Cat_Priority = 2, Categories=categories2};
-            cat14 = new Category() { Cat_CreateTime = DateTime.Now, Cat_Title = "Category14", Cat_NodeDepth = 1, Cat_Priority = 6 };
+            cat13 = new Category() { Cat_CreateTime = DateTime.Now, Cat_Title = "Category13", Cat_NodeDepth = 1, Cat_Priority = 2, Categories = categories2 };
+            cat14 = new Category() { Cat_CreateTime = DateTime.Now, Cat_Title = "Category14", Cat_NodeDepth = 1, Cat_Priority = 3 };
             categories1.Add(cat12);
             categories1.Add(cat13);
             categories1.Add(cat14);
-            cat131 = new Category() { Cat_CreateTime = DateTime.Now, Cat_Title = "Category131", Cat_NodeDepth = 2, Cat_Priority = 1};
+            cat131 = new Category() { Cat_CreateTime = DateTime.Now, Cat_Title = "Category131", Cat_NodeDepth = 2, Cat_Priority = 1 };
             cat132 = new Category() { Cat_CreateTime = DateTime.Now, Cat_Title = "Category132", Cat_NodeDepth = 2, Cat_Priority = 2 };
             categories2.Add(cat131);
             categories2.Add(cat132);
-            //object parameters=null;
-            //object[] parameterArray = (object[])parameters;
             context.Categories.Add(cat1);
 
             base.Seed(context);
--- ERPInventory/Global.asax.cs
+++ ERPInventory/Global.asax.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
@@ -24,11 +25,30 @@ namespace ERPInventory
             var context = new ERPInventoryDBContext();
             context.Database.Initialize(true);
 
-            var result = context.Database.SqlQuery<Category>("GetParentCategoryByID @ID",
-                new SqlParameter("ID", Guid.Parse("53097d2d-a537-e511-b770-002433726434"))
-                ).ToList();
-           int c = result.Count();
-           string s = result.FirstOrDefault().Cat_Title;
+           // var result = context.Database.SqlQuery<Category>("GetParentCategoryByID @ID",
+           //     new SqlParameter("ID", Guid.Parse("53097d2d-a537-e511-b770-002433726434"))
+           //     ).ToList();
+           //int c = result.Count();
+           //string s = result.FirstOrDefault().Cat_Title;
+
+           StringBuilder sb = new StringBuilder();
+
+           sb.Append("create FUNCTION [dbo].[AllSubCategoryId2](@CategoryId uniqueIdentifier )");
+           sb.Append(" RETURNS TABLE");
+           sb.Append(" AS");
+           sb.Append(" RETURN (WITH CategoryChart (CategoryId,Cat_ParentId,CategoryLevel) AS");
+           sb.Append(" (");
+           sb.Append(" SELECT");
+           sb.Append(" a.CategoryId,  a.Cat_ParentId,0");
+           sb.Append(" FROM dbo.Categories as a");
+           sb.Append(" WHERE a.CategoryId 	= @CategoryId");
+           sb.Append(" UNION ALL");
+           sb.Append(" SELECT");
+           sb.Append(" a.CategoryId,  a.Cat_ParentId, b.Categorylevel+1");
+           sb.Append(" FROM dbo.Categories a");
+           sb.Append(" INNER JOIN CategoryChart b ON a.Cat_ParentId = b.CategoryId");
+           sb.Append(" ) select CategoryId from CategoryChart );");
+           string sss = sb.ToString();
         }
     }
 }
