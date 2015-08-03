3656356c5b4da3db016e4a8ae32cd00135500a38
kaveh fereidouni <kaveh981@gmail.com>
2015-08-03 12:14:39 -0400

    jsonIgnore added

--- ERPInvenory.Model/Models/inv_Category.cs
+++ ERPInvenory.Model/Models/inv_Category.cs
@@ -1,4 +1,5 @@
 ﻿
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -29,10 +30,10 @@ namespace ERPInventory.Model.Models
         public DateTime? Cat_StampTime { get; set; }
 
 
-       
-        public virtual  inv_Category Parent { get; set; }
+        [JsonIgnore]
+        public virtual inv_Category Parent { get; set; }
 
-       
+        [JsonIgnore]
         public virtual ICollection<inv_Category> Categories { get; set; }
 
 
