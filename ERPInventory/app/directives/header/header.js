7472eba32e0a782aa9928447f39812700ab515f0
kaveh fereidouni <kaveh981@gmail.com>
2015-08-11 01:02:12 -0400

    d

---
+++ ERPInventory/app/directives/header/header.js
@@ -0,0 +1,35 @@
+﻿
+angular.module('ERPInventory').controller('headerController', function ($scope, $modal) {
+
+    $scope.showLoginForm = function () {
+
+        var modalInstance = $modal.open({
+
+            templateUrl: 'app/views/accounts/signIn.html',
+            controller: 'signInController',
+        });
+        
+    };
+
+    $scope.showRegisterForm = function () {
+
+        var modalInstance = $modal.open({
+
+            templateUrl: 'app/views/accounts/addUsers.html',
+            controller: 'accountController',
+
+        });
+
+    };
+
+
+
+});
+
+angular.module('ERPInventory').directive('erpHeader', function () {
+    return {
+        templateUrl: function (elem, attr) {
+            return 'app/directives/header/header.html';
+        }
+    };
+});
